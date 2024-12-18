eece5c0a31c49198a986572a849df05d98d19204
Eduardo Rocha <86627559+Duuhwow@users.noreply.github.com>
2024-12-18 12:04:01 -0300

    Adição dos exercícios. 18/12/2024
    Criar um diretório próprio para eles.

---
+++ exe002.py
@@ -0,0 +1,6 @@
+# =============== Exercicio 002 ===============
+#Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+
+print("=============== Exercicio 002 ===============")
+nome = input('Digite seu Nome:')
+print(f'Bem vindo(a), {nome} !')
